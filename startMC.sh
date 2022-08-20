8581b3cc699e7a2947a98bcc56b4911283096433
Albert Garangou Culebras <albertgarangou@gmail.com>
2022-08-20 13:30:41 +0200

    added file for just starting after boot

---
+++ startMC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! screen -list | grep -q mcs;
+  then
+    screen -d -m -S mcs java -Xms2G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui
+  else
+    echo [MC Server] Ja està obert, no es tornarà a obrir.
+fi
